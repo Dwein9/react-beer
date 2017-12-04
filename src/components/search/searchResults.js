bf47093c461ed459a9f3e2874fc94a5860ba0cd7
dwein9 <dwein9@gmail.com>
2017-12-04 16:38:10 -0500

    results display styling

--- src/components/search/searchResults.js
+++ src/components/search/searchResults.js
@@ -3,12 +3,27 @@ import React, { Component } from 'react'
 export default class searchResults extends Component {
 
   displayResults = () => {
+    const styleList = { float: 'left',
+      position: 'relative',
+      width: '30%',
+      'padding-bottom': '30%',
+      margin: '1.66%',
+      overflow: 'hidden'
+    }
+
+    const stylePhoto = {
+      position: 'absolute',
+      height:' 80%',
+      width: '90%',
+      padding: '10% 5%',
+    }
+
     let results = this.props.results.map((result, i) => {
       return (
-        <li style={{'list-style-type': 'none'}} key={i}>
-          <img src={result.img} alt=""/>
+        <div style={styleList} key={i}>
+          <img style={stylePhoto} src={result.img} alt=""/>
           <p>{result.name}</p>
-        </li>
+        </div>
       )
     })
     return results
